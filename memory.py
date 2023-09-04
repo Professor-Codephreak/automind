34c54a84f5985ef222c65e5a8c1f5345cce17eab
codephreak <140855987+Professor-Codephreak@users.noreply.github.com>
2023-09-04 13:25:50 -0700

    Update memory.py
    'DialogEntry class to encapsulate dialog entries and added a to_dict method to convert instances to dictionaries.
    Added folder and file permissions (0o700 for folders and 0o600 for files) to make them only accessible by the user who created them.
    Added try-except blocks to handle errors and used Python's built-in logging library to log any exceptions that occur.

--- memory.py
+++ memory.py
@@ -1,54 +1,50 @@
-#memory.py
 import os
 import pathlib
 import time
 import ujson
+import logging
 
 # Define the constant for memory folder
 MEMORY_FOLDER = "./memory/"
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
 
 class DialogEntry:
     def __init__(self, instruction, response):
         self.instruction = instruction
         self.response = response
 
-def save_conversation_memory(memory):
-    # Create the folder if it doesn't exist
-    if not pathlib.Path(MEMORY_FOLDER).exists():
-        pathlib.Path(MEMORY_FOLDER).mkdir(parents=True)
-
-    # Generate a unique filename based on the timestamp
-    timestamp = str(int(time.time()))  # Use the current timestamp as the filename
-    filename = f"memory_{timestamp}.json"
-    memory_file_path = pathlib.Path(MEMORY_FOLDER).joinpath(filename)
-
-    # Format the conversation memory as a list of DialogEntry objects
-    memory_records = [DialogEntry(dialog[0], dialog[1]) for dialog in memory]
-
-    # Convert DialogEntry objects to dictionaries
-    memory_records_dicts = [{"instruction": entry.instruction, "response": entry.response} for entry in memory_records]
-
-    # Save the memory to the JSON file using ujson
-    with open(memory_file_path, "w", encoding="utf-8") as file:
-        ujson.dump(memory_records_dicts, file, ensure_ascii=False, indent=2)
-
-    return memory_file_path
+    def to_dict(self):
+        return {
+            "instruction": self.instruction,
+            "response": self.response
+        }
 
 def save_conversation_memory(memory):
-    # Create the folder if it doesn't exist
-    if not pathlib.Path(MEMORY_FOLDER).exists():
-        pathlib.Path(MEMORY_FOLDER).mkdir(parents=True)
+    try:
+        # Create the folder if it doesn't exist
+        folder_path = pathlib.Path(MEMORY_FOLDER)
+        if not folder_path.exists():
+            folder_path.mkdir(parents=True, mode=0o700)  # Secure folder permissions
 
-    # Generate a unique filename based on the timestamp
-    timestamp = str(int(time.time()))  # Use the current timestamp as the filename
-    filename = f"memory_{timestamp}.json"
-    file_path = pathlib.Path(MEMORY_FOLDER).joinpath(filename)
+        # Generate a unique filename based on the timestamp
+        timestamp = str(int(time.time()))
+        filename = f"memory_{timestamp}.json"
+        file_path = folder_path.joinpath(filename)
 
-    # Format the conversation memory as a list of dictionaries
-    formatted_memory = [{"instruction": dialog[0], "response": dialog[1]} for dialog in memory]
+        # Format the conversation memory as DialogEntry objects
+        formatted_memory = [DialogEntry(dialog[0], dialog[1]).to_dict() for dialog in memory]
 
-    # Save the memory to the JSON file using ujson
-    with open(file_path, "w", encoding="utf-8") as file:
-        ujson.dump(formatted_memory, file, ensure_ascii=False, indent=2)
+        # Save the memory to the JSON file using ujson
+        with open(file_path, "w", encoding="utf-8") as file:
+            ujson.dump(formatted_memory, file, ensure_ascii=False, indent=2)
 
-    return file_path
+        # Secure file permissions
+        os.chmod(file_path, 0o600)
+
+        return file_path
+
+    except Exception as e:
+        logging.error(f"An error occurred while saving the conversation memory: {e}")
+        return None
